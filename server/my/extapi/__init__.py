e5b896c7afa72ba0616ce7979a53ef31dd3089fb
blashyrkh <blashyrkh@inbox.ru>
2020-07-11 23:30:48 +0300

    MCI_tokenContent handler

--- server/my/extapi/__init__.py
+++ server/my/extapi/__init__.py
@@ -8,7 +8,7 @@ def register(*, db, config, registry):
     ethcontract.register(db=db, config=config, registry=registry)
 
 def customResultHandler(name):
-    if name=="mint":
+    if name=="MCI_mint":
         def mintResultHandler(receipt, result):
             for logEntry in receipt["logs"]:
                 if len(logEntry["topics"])>1 and logEntry["topics"][0]=="0xea630aa0f059e408d9549df47528546a0da45532fa05f28a90600a8617e3be1e":
@@ -17,5 +17,13 @@ def customResultHandler(name):
             return result
 
         return mintResultHandler
+
+    elif name=="MCI_tokenContent":
+        def tokenContentResultHandler(result):
+            if len(result)==2+64*5:
+                return ["0x"+result[2+64*i:2+64*(i+1)] for i in range(5)]
+            return []
+
+        return tokenContentResultHandler
 
     return None
--- server/my/extapi/ethcontract.py
+++ server/my/extapi/ethcontract.py
@@ -5,6 +5,54 @@
 from .. import ethconfig
 from .. import ethqueue
 from .. import ethfee
+from ..nodepool import *
+from .. import cgijsonrpc
+from .. import extapi
+
+def makeTransactionData(selector, argtypes, args):
+    if not isinstance(args, list):
+        raise TypeError('"args" argument must be an array')
+
+    if len(args)!=len(argtypes):
+        raise TypeError("{0} argument(s) expected, {1} given".format(len(argtypes), len(args)))
+
+    txdata=bytes.fromhex(selector)
+    dynpos=0
+    for argtype in argtypes:
+        if argtype in ("address", "uint256"):
+            dynpos+=32
+        elif argtype=="string":
+            dynpos+=32
+        else:
+            raise TypeError('Serialization of "{0}" type is not implemented'.format(argtype))
+
+    tail=b""
+    for argtype, argvalue in zip(argtypes, args):
+        if argtype=="address":
+            if not isinstance(argvalue, str):
+                raise TypeError('Argument of "address" type must be passed as a string')
+
+            txdata+=12*b'\0'+bytes.fromhex(argvalue[2:])
+
+        elif argtype=="uint256":
+            if not isinstance(argvalue, str):
+                raise TypeError('Argument of "uint256" type must be passed as a string')
+
+            txdata+=int(argvalue, 0).to_bytes(32, "big")
+
+        elif argtype=="string":
+            txdata+=dynpos.to_bytes(32, "big")
+
+            argvalue=argvalue.encode("utf8")
+
+            tail+=len(argvalue).to_bytes(32, "big")
+            dynpos+=32
+
+            padded=argvalue+b'\0'*((32-len(argvalue)%32)%32)
+            tail+=padded
+            dynpos+=len(padded)
+
+    return txdata+tail
 
 class FunctionInvocation:
     def __init__(self, *, db, config, chainid, contract, sender, selector, argtypes, customResultHandler):
@@ -29,58 +77,41 @@ class FunctionInvocation:
             **self.__makeTxArgs(args))
 
     def __makeTxArgs(self, args):
-        if not isinstance(args, list):
-            raise TypeError('"args" argument must be an array')
-
-        if len(args)!=len(self.argtypes):
-            raise TypeError("{0} argument(s) expected, {1} given".format(len(self.argtypes), len(args)))
-
-        txdata=bytes.fromhex(self.selector)
-        dynpos=0
-        for argtype in self.argtypes:
-            if argtype in ("address", "uint256"):
-                dynpos+=32
-            elif argtype=="string":
-                dynpos+=32
-            else:
-                raise TypeError('Serialization of "{0}" type is not implemented'.format(argtype))
-
-        tail=b""
-        for argtype, argvalue in zip(self.argtypes, args):
-            if argtype=="address":
-                if not isinstance(argvalue, str):
-                    raise TypeError('Argument of "address" type must be passed as a string')
-
-                txdata+=12*b'\0'+bytes.fromhex(argvalue[2:])
-
-            elif argtype=="uint256":
-                if not isinstance(argvalue, str):
-                    raise TypeError('Argument of "uint256" type must be passed as a string')
-
-                txdata+=int(argvalue, 0).to_bytes(32, "big")
-
-            elif argtype=="string":
-                txdata+=dynpos.to_bytes(32, "big")
-
-                argvalue=argvalue.encode("utf8")
-
-                tail+=len(argvalue).to_bytes(32, "big")
-                dynpos+=32
-
-                padded=argvalue+b'\0'*((32-len(argvalue)%32)%32)
-                tail+=padded
-                dynpos+=len(padded)
-
-        txdata+=tail
-
         return dict(
             chainid=self.chainid,
             sender=self.sender,
             receiver=self.contract,
             value=0,
-            data=txdata,
+            data=makeTransactionData(self.selector, self.argtypes, args),
             customResultHandler=self.customResultHandler)
 
+class Call:
+    def __init__(self, *, config, chainid, contract, selector, argtypes, customResultHandler):
+        self.config=config
+        self.chainid=chainid
+        self.contract=contract
+        self.selector=selector
+        self.argtypes=argtypes
+        self.customResultHandler=customResultHandler
+
+    def __call__(self, args):
+        nodepool=NodePool(config=self.config)
+        node=nodepool.connectToAnyNode(self.chainid)
+        if node is None:
+            raise cgijsonrpc.JsonRPCException(-1003, "ETH node is unavailable")
+
+        data=makeTransactionData(self.selector, self.argtypes, args)
+        res=node.eth_call(dict(to=self.contract, data="0x"+data.hex()), "latest")
+
+        if self.customResultHandler is not None:
+            customResultHandler=extapi.customResultHandler(self.customResultHandler)
+            if customResultHandler:
+                res=customResultHandler(res)
+
+        return res
+
+    def estimateFee(self, args):
+        return 0
 
 def register(*, db, config, registry):
     for coinDescription in ethconfig.listCoinDescriptions(config):
@@ -95,6 +126,17 @@ def register(*, db, config, registry):
                             chainid=coinDescription.chainid,
                             contract=coinDescription.contract,
                             sender=apiDescription["sender"],
+                            selector=apiDescription["selector"],
+                            argtypes=apiDescription["args"],
+                            customResultHandler=apiDescription.get("customResult")))
+
+                elif apiDescription["type"]=="call":
+                    registry.addMethod(
+                        apiDescription["name"],
+                        Call(
+                            config=config,
+                            chainid=coinDescription.chainid,
+                            contract=coinDescription.contract,
                             selector=apiDescription["selector"],
                             argtypes=apiDescription["args"],
                             customResultHandler=apiDescription.get("customResult")))
